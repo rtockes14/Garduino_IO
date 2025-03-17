699b9b6f1c5da8297785264f784aa595da59e0b9
Randall <randalltockes@gmail.com>
2025-03-17 11:34:21 -0500

    Adding Libraries

---
+++ ESP32_Garduino-Firmware/.pio/build/esp32dev/lib949/WebServer/detail/mimetable.cpp.d
@@ -0,0 +1,4 @@
+.pio/build/esp32dev/lib949/WebServer/detail/mimetable.cpp.o: \
+ C:/Users/Randa/.platformio/packages/framework-arduinoespressif32/libraries/WebServer/src/detail/mimetable.cpp \
+ C:/Users/Randa/.platformio/packages/framework-arduinoespressif32/libraries/WebServer/src/detail/mimetable.h \
+ C:/Users/Randa/.platformio/packages/framework-arduinoespressif32/cores/esp32/pgmspace.h
